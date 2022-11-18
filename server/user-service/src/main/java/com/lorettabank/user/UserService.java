5ad4788a9f3b29d51a4550858143841f9e768105
7irelo <tirelo.eric@gmail.com>
2022-11-18 14:22:00 +0200

    feat(server): scaffold microservice modules from node prototype

---
+++ server/user-service/src/main/java/com/lorettabank/user/UserService.java
@@ -0,0 +1,16 @@
+
+package com.lorettabank.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
