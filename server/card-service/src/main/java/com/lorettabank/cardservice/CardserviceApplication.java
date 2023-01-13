5065506a36809a09192942aceb06d2791b47c80c
7irelo <tirelo.eric@gmail.com>
2023-01-13 16:44:00 +0200

    chore(config): refine datasource and environment settings

---
+++ server/card-service/src/main/java/com/lorettabank/cardservice/CardserviceApplication.java
@@ -0,0 +1,13 @@
+package com.lorettabank.cardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardserviceApplication.class, args);
+	}
+
+}
---
+++ server/card-service/src/test/java/com/lorettabank/cardservice/CardserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorettabank.cardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
