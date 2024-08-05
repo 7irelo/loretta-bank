3b6e69f871fe24e1cd916925e7ebb1df688329f7
Eric Ncube <tirelo.eric@gmail.com>
2024-08-05 12:18:57 +0200

    TRANSFER FROM NODE

---
+++ server/src/main/java/com/lorettabank/LorettaBankApplication.java
@@ -0,0 +1,13 @@
+package com.lorettabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorettaBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorettaBankApplication.class, args);
+	}
+
+}
---
+++ server/user-service/src/main/java/com/lorettabank/user/UserController.java
@@ -0,0 +1,20 @@
+
+package com.lorettabank.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+}
